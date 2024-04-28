4e6352828c07a1cca48ffd2e2cc1869a8591e8c3
Irene907 <iren80907@gmail.com>
2024-04-29 00:36:15 +0800

    預約訂房

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// header
 window.addEventListener("scroll",function(){
     let nav = document.querySelector('#nav');
     let mainNav = document.querySelectorAll('#main-nav a');
@@ -11,11 +12,28 @@ window.addEventListener("scroll",function(){
     } else {
         nav.style.backgroundColor = "rgba(0,0,0,0)";
         for (let i = 0; i<mainNav.length; i++) {
-            mainNav[i].style.color = "white";
+            mainNav[i].style.color = "222222";
         }
     }
     if (scrollY>100){
         logo.src="./images/logo-green.png";
     }else {logo.src="./images/logo.png";}
     
+});
+
+//booknow內頁
+
+const tabs = document.querySelectorAll('.tab_box button');
+const contents = document.querySelectorAll('.content_box .content');
+
+// 預設顯示第一個內容
+// contents[0].classList.add('active');
+
+tabs.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+        contents.forEach(content => {
+            content.classList.remove('active');
+        });
+        contents[index].classList.add('active');
+    });
 });
