e60f9189d8fb16a24c792632dac2d24e4cced26e
Irene907 <iren80907@gmail.com>
2024-05-01 17:54:52 +0800

    預約訂房tab+手機版nav調整

--- index.js
+++ index.js
@@ -1,41 +1,69 @@
+window.addEventListener('load', function (){
+    //-----AOS
+    window.addEventListener("scroll",function(){
+        navAnimate()    
+    });
+    //----booknow內頁
+    const tabs = document.querySelectorAll('.tab_btn');
+    console.log(tabs);
+    for (let i = 0; i<tabs.length; i++){
+        tabs[i].onclick =function () {
+            let tag = Array.prototype.indexOf.call(tabs, this);
+            const contents = document.querySelectorAll('.content_box .content');
+            for(let i =0; i<contents.length; i++){
+                contents[i].classList.remove('active',)
+                tabs[i].classList.remove('highlight')
+            }
+            contents[tag].classList.add('active')
+            tabs[tag].classList.add('highlight')
+        }
+    }
+})
+
+window.addEventListener('load', function() {
+    const hb = document.getElementById('hb');
+
+    function changeHbColor() {
+        if (window.pageYOffset > 10) {
+        hb.style.color = "#222222";
+    } else {
+        hb.style.color = "white";
+    }
+    }
+
+    window.addEventListener('scroll', changeHbColor);
+    changeHbColor(); // 初始化顏色
+});
+
+
 // header
-window.addEventListener("scroll",function(){
+function navAnimate(){
     let nav = document.querySelector('#nav');
     let mainNav = document.querySelectorAll('#main-nav a');
     let logo = document.getElementById('logo');
-    
+
     if (scrollY > 0) {
         nav.style.backgroundColor = "white";
         for (let i = 0; i<mainNav.length; i++) {
             mainNav[i].style.color = "#222222";
         }
     } else {
-        nav.style.backgroundColor = "rgba(0,0,0,0)";
+        nav.style.backgroundColor = "rgba(0,0,0,0.5)";
         for (let i = 0; i<mainNav.length; i++) {
-            mainNav[i].style.color = "#222222";
+            mainNav[i].style.color = "white";
         }
     }
     if (scrollY>10){
         logo.src="./images/logo-green.png";
-    }else {logo.src="./images/logo.png";}
+    }else {
+        logo.src="./images/logo.png";
+    }
 
-    
-    
-});
+    if (scrollY>10){
+        hb.style.color="#222222";
+    }else {
+        hb.style.color="white";
+    }
 
-//booknow內頁
+}
 
-const tabs = document.querySelectorAll('.tab_box button');
-const contents = document.querySelectorAll('.content_box .content');
-
-
-contents[0].classList.add('active');
-
-tabs.forEach((tab, index) => {
-    tab.addEventListener('click', () => {
-        contents.forEach(content => {
-            content.classList.remove('active');
-        });
-        contents[index].classList.add('active');
-    });
-});
