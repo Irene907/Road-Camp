23df7b30b69de61b22a395f0220fc1f63d42704d
Irene <iren870907@gmail.com>
2024-08-14 23:15:15 +0800

    增加登入功能

--- login.js
+++ login.js
@@ -2,14 +2,26 @@ const loginBtn = document.querySelector('button');
 const usernameInput = document.querySelector('.login-ac input');
 const passwordInput = document.querySelector('.login-psw input');
 
-loginBtn.addEventListener('click', () => {
-const username = usernameInput.value;
-const password = passwordInput.value;
+if (localStorage.getItem('isLogin') === 'true') {
+    window.location.href = 'index.html';
+};
 
-if (username === 'a123' && password === '123123') {
-    alert('登入成功！');
-    
-} else {
-    alert('帳號或密碼錯誤，請重新輸入。');
-}
+loginBtn.addEventListener('click', () => {
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+
+    if (username === 'a123' && password === '123123') {
+        alert('登入成功！');
+
+        //儲存localstorage資料
+        localStorage.setItem('isLogin', 'true');
+        console.log('isLogin');
+
+
+        window.location.href = 'index.html';
+
+    } else {
+        alert('帳號或密碼錯誤，請重新輸入。');
+    }
 });
+
